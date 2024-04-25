3b2c96640f682ec037801451578103168f4d5283
unknown <marcos.benitez@alu.inspt.utn.edu.ar>
2024-04-25 18:05:50 -0300

    Ej3

---
+++ extrasEj2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+float precio, plan1, plan2, plan3, plan4, aux2, aux3, aux4;
+int n;
+
+printf("Ingrese el precio del articulo:\n");
+scanf("%f, &precio");
+
+plan1 = precio * 0.9;
+n=1;
+
+printf("Plan1 paga: %f",   "nro. de cuotas:" ""   %d  ",plan1, n );
+
+plan2= (precio*1.1) / 2;
+n=2;
+
+printf("Plan2 paga: %f       ");
+
+
+
+
+
+
+
+}
